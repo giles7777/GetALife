68b2c853a6ab004393a3615fa93297fc44d89e2a
upliftingtech <77591525+upliftingtech@users.noreply.github.com>
2021-03-10 18:27:43 -0800

    remove wiring section from node_13 and bridge_13
    no longer needed. now in wiring.md

--- nodes/Bridge_v13/Bridge_v13.ino
+++ nodes/Bridge_v13/Bridge_v13.ino
@@ -3,19 +3,6 @@
 // CPU Frequency: 80 MHz
 // Flash Size: 16 Mb (~1Mb OTA)
 
-// Wiring (updated 2/14/21)
-// Used: A0:batt, D0:sleep, D4:builtin led, D5:buzzer, D7:LED, D2:PIR, D6:(reserved)
-// 
-// D1 mini pro: bridge BAT-A0, bridge SLEEP; connect +5, +3.3, GND, D5, D7.  
-// LED: bridge D7 (not D4 default); connect +5, +3.3, GND, D7.
-// Buzzer: none (D5 default);  connect +5, +3.3, GND, D5.
-///
-// I2C Cable: D1 (SCL) D2 (SDA)
-//    PIR: bridge D2 (not D3 default)
-//
-// deprecated:
-// Relay: bridge D6 (not D1 default)
-
 // change these to you home SSID
 #define   STATION_SSID     "Looney_Bin"
 #define   STATION_PASSWORD "TinyandTooney"
